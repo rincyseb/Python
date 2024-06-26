38332cf29ea49f0a303d98d7ad634b1547f9baf3
rincyseb <165606979+rincyseb@users.noreply.github.com>
2024-06-25 22:14:25 -0500

    Add files via upload

---
+++ Collections.py
@@ -0,0 +1,23 @@
+#List
+myFruitList = ["Apple","Banana", "Cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "Orange"
+print(myFruitList)
+#Tuple
+myFinalAnswerTuple =("Apple","Banana","Pinapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+#Dictionary
+myFavoriteFruitDictionary = { "Akua" : "Apple", "Saanvi" : "Banana", "Paulo" : "Pinapple"}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
---
+++ string-data-type.py
@@ -0,0 +1,16 @@
+myString = "This is a string."
+print(myString)
+print(myString + " is of the data type " + str(type(myString)))
+#StringConcat
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+#user input
+name = input("What is your name? ")
+print(name)
+color= input("what is your favorite color? ")
+print(color)
+animal= input("What is your favourite Animal? ")
+print(animal)
+print("{}, you like {} color, {} animal".format(name,color,animal))
